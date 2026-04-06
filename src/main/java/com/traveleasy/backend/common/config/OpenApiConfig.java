cdf0e310815565a12575e0c22bd08776b724a810
Roman <romanserbul@gmail.com>
2026-04-06 20:06:15 +0300

    feat: enhance OpenAPI configuration with dynamic server URLs

--- src/main/java/com/traveleasy/backend/common/config/OpenApiConfig.java
+++ src/main/java/com/traveleasy/backend/common/config/OpenApiConfig.java
@@ -5,11 +5,18 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
 public class OpenApiConfig {
+
+    @Value("${app.url:https://api.easy-travel.com.ua}")
+    private String appUrl;
 
     @Bean
     public OpenAPI travelEasyOpenApi() {
@@ -17,9 +24,13 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Travel Easy API")
                         .description("Backend services for the Travel Easy booking platform")
-                .version("v0.1"))
-            .components(new Components().addSecuritySchemes("basicAuth",
-                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
-            .addSecurityItem(new SecurityRequirement().addList("basicAuth"));
+                        .version("v0.1"))
+                .servers(List.of(
+                        new Server().url(appUrl).description("Production"),
+                        new Server().url("http://localhost:8081").description("Local Development")
+                ))
+                .components(new Components().addSecuritySchemes("basicAuth",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+                .addSecurityItem(new SecurityRequirement().addList("basicAuth"));
     }
 }
